50abec34af89de95c393c3a8ea12acf00c9821f8
PoojaGadadare <gadadarepooja@gmail.com>
2023-05-04 18:56:59 +0530

    Done

---
+++ Operators.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=4;
+int b=9;
+int c=8;
+int x= a>b?(a>c?a:c):(b>c?b:c);
+System.out.println(x);
+
+	}
+
+}
